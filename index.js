3497c649cf359e488b5f37b0e490d6722ca3e628
Said-akbar-7 <nabixojaevsaidakbar@gmail.com>
2022-03-01 15:44:07 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let answers = confirm("Qatnashishni hoxlaysizmi ?") ;
+console.log(answers)
+
+let carName = prompt("Moshinani nomi nima ?");
+console.log(carName)
+
+let carColor = prompt("Rangi qanaqa ?");
+console.log(carColor)
+
+let carSale = prompt("Narxi qancha ?");
+console.log(carSale)
+
+let where = confirm("Chetelnikimi ?");
+console.log(where)
+
+
+if (carSale < 10000) {
+    console.log("Moshina arzon ekan !")
+}
+
+else if (carSale == 10000) {
+    console.log("Olsa bo'ladi")
+}
+
+else if (carSale > 10000) {
+    console.log("Moshina biroz qimmatroq ekan")
+}
+
+
+
+
+
+
+
+
+// if (hisob == 0) {
+//     console.log("Juft Son");
+// }
